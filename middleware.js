d43bbaa8e6b09e7771f3ed22f20a36c81dff097e
siraku <slstc6666@yahoo.co.jp>
2025-03-08 11:17:58 +0900

    add clerk

---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default clerkMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: ["/api/webhook"]
+});
+
+export const config = {
+  matcher: ["/((?!.+\.[\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
